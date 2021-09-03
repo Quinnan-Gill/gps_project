4d8d79b35ce9fab70d9fa6bdb359bed883685af8
Quinnan Gill <quinnan.gill@gmail.com>
2021-09-03 14:10:25 -0400

    Added modules

--- src/rnn_modules.py
+++ src/rnn_modules.py
@@ -82,4 +82,143 @@ class LSTMCell(nn.Module):
         return
 
 
-# class Visualize
+class PeepholedLSTMCell(nn.Module):
+
+    def __init__(self, input_size, hidden_size, bias=False):
+        super().__init__()
+
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.bias = bias
+
+        #####################################################################
+        # Learnable weights and bias for `foreget gate`
+        self.W_f = nn.Parameter(torch.Tensor(hidden_size, 2*hidden_size + input_size))
+        # Implement here following the given signature                                            #
+        self.W_i = nn.Parameter(torch.Tensor(hidden_size, 2*hidden_size + input_size))
+        # Learnable weights and bias for `output gate`
+        self.W_o = nn.Parameter(torch.Tensor(hidden_size, 2*hidden_size + input_size))
+        # Learnable weights and bias for `new memory cell`
+        self.W_c = nn.Parameter(torch.Tensor(hidden_size, hidden_size + input_size))
+
+        if bias:
+            self.b_f = nn.Parameter(torch.Tensor(hidden_size))
+            self.b_i = nn.Parameter(torch.Tensor(hidden_size))
+            self.b_o = nn.Parameter(torch.Tensor(hidden_size))
+            self.b_c = nn.Parameter(torch.Tensor(hidden_size))
+        else:
+            self.register_parameter('b_f', None)
+            self.register_parameter('b_i', None)      
+            self.register_parameter('b_o', None)
+            self.register_parameter('b_c', None)
+        
+        self.count_parameters()
+        self.reset_parameters()
+        #####################################################################
+
+        return
+
+    def forward(self, x, prev_state):
+        # #####################################################################
+        if prev_state is None:
+            batch = x.shape[0]
+            prev_h = torch.zeros((batch, self.hidden_size), device=x.device)
+            prev_c = torch.zeros((batch, self.hidden_size), device=x.device)
+        else:
+            prev_h, prev_c = prev_state
+        
+        prev_concat_hxc = torch.cat((prev_c, prev_h, x), dim=1)
+        f = torch.sigmoid(F.linear(prev_concat_hxc, self.W_f, self.b_f))
+        i = torch.sigmoid(F.linear(prev_concat_hxc, self.W_i, self.b_i))
+
+        concat_hx = torch.cat((prev_h, x), dim=1)
+        c_tilde = torch.tanh(F.linear(concat_hx, self.W_c, self.b_c))
+
+        next_c = f * prev_c + i * c_tilde
+        next_concat_hxc = torch.cat((next_c, prev_h, x), dim=1)
+        o = torch.sigmoid(F.linear(next_concat_hxc, self.W_o, self.b_o))
+
+        next_h = o * torch.tanh(next_c)
+        # #####################################################################
+        return next_h, next_c
+
+    def reset_parameters(self):
+        sqrt_k = (1. / self.hidden_size)**0.5
+        with torch.no_grad():
+            for param in self.parameters():
+                param.uniform_(-sqrt_k, sqrt_k)
+        return
+
+    def extra_repr(self):
+        return 'input_size={}, hidden_size={}, bias={}'.format(
+                self.input_size, self.hidden_size, self.bias is not True)
+
+    def count_parameters(self):
+        print('Total Parameters: %d' %
+                    sum(p.numel() for p in self.parameters() if p.requires_grad))
+        return
+
+class CoupledLSTMCell(nn.Module):
+
+    def __init__(self, input_size, hidden_size, bias=False):
+        super().__init__()
+
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.bias = bias
+
+        #####################################################################
+        # Learnable weights and bias for `foreget gate`
+        self.W_f = nn.Parameter(torch.Tensor(hidden_size, hidden_size + input_size))
+        # Learnable weights and bias for `output gate`
+        self.W_o = nn.Parameter(torch.Tensor(hidden_size, hidden_size + input_size))
+        # Learnable weights and bias for `new memory cell`
+        self.W_c = nn.Parameter(torch.Tensor(hidden_size, hidden_size + input_size))
+        if bias:
+            self.b_f = nn.Parameter(torch.Tensor(hidden_size))
+            self.b_o = nn.Parameter(torch.Tensor(hidden_size))
+            self.b_c = nn.Parameter(torch.Tensor(hidden_size))
+        else:
+            self.register_parameter('b_f', None)
+            self.register_parameter('b_o', None)
+            self.register_parameter('b_c', None)
+        
+        self.count_parameters()
+        self.reset_parameters()
+        #####################################################################
+
+        return
+
+    def forward(self, x, prev_state):
+        #####################################################################
+        if prev_state is None:
+            batch = x.shape[0]
+            prev_h = torch.zeros((batch, self.hidden_size), device=x.device)
+            prev_c = torch.zeros((batch, self.hidden_size), device=x.device)
+        else:
+            prev_h, prev_c = prev_state
+        
+        concat_hx = torch.cat((prev_h, x), dim=1)
+        f = torch.sigmoid(F.linear(concat_hx, self.W_f, self.b_f))
+        o = torch.sigmoid(F.linear(concat_hx, self.W_o, self.b_o))
+        c_tilde = torch.tanh(F.linear(concat_hx, self.W_c, self.b_c))
+        next_c = f * prev_c + (1-f) * c_tilde
+        next_h = o * torch.tanh(next_c)
+        #####################################################################
+        return next_h, next_c
+
+    def reset_parameters(self):
+        sqrt_k = (1. / self.hidden_size)**0.5
+        with torch.no_grad():
+            for param in self.parameters():
+                param.uniform_(-sqrt_k, sqrt_k)
+        return
+
+    def extra_repr(self):
+        return 'input_size={}, hidden_size={}, bias={}'.format(
+                self.input_size, self.hidden_size, self.bias is not True)
+
+    def count_parameters(self):
+        print('Total Parameters: %d' %
+                    sum(p.numel() for p in self.parameters() if p.requires_grad))
+        return
