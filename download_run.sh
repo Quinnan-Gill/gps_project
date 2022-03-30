c913836c8c65e854300e76937692aa4d7b0bb5a9
Quinnan Gill <quinnan.gill@gmail.com>
2022-03-29 22:33:39 -0400

    Fixed dates

--- download_run.sh
+++ download_run.sh
@@ -1,4 +1,4 @@
 for VARIABLE in {1..1000}
 do
-    python src/expanded_datasets.py --ds_start_val_time 2016_02_01 --ds_end_val_time 2016_02_29 --fetch_data
+    python src/expanded_datasets.py --ds_start_val_time 2016_02_01 --ds_end_val_time 2017_02_29 --fetch_data
 done
